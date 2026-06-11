c898a03aa0dc12906606c72fa6356dfa2dab22ca
Sandy <codingsandy6@gmail.com>
2026-06-11 23:52:36 +0545

    Fixes some issue in the server

--- server/todo/views.py
+++ server/todo/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .serializers import TodoSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Todo
 
-# Create your views here.
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
